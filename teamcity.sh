48cd4ff9baedcb434bbc0ba73d9c78e604c1a37f
Akash A <akash1810@gmail.com>
2018-08-17 16:44:01 +0100

    invalidate cloudfront in deploy step
    also running all build steps in one scripts makes it easier to test in DEV

--- teamcity.sh
+++ teamcity.sh
@@ -1,6 +1,38 @@
 #!/usr/bin/env bash
+
+NODE_VERSION="8"
+
+CLOUDFRONT_DISTRIBUTION=E16PAH0C37EAGC
+
 BRANCH=$1
-if [ "$BRANCH" = "master" ]
-then
-  aws s3 sync build/ s3://tools-gutools-co-uk
-fi
+
+nvm() {
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+    nvm use ${NODE_VERSION}
+}
+
+build() {
+    npm install
+    npm run build
+}
+
+deploy() {
+    aws s3 sync build/ s3://tools-gutools-co-uk
+
+    aws cloudfront create-invalidation \
+        --distribution-id ${CLOUDFRONT_DISTRIBUTION} \
+        --paths /\*
+}
+
+main() {
+    nvm
+    build
+
+    if [ "$BRANCH" = "master" ]
+    then
+        deploy
+    fi
+}
+
+main
